120d16ef1187e51052b205afdc9fb1c2d5aef159
August Lilleaas <august@augustl.com>
2016-01-09 19:16:52 +0100

    Update dev repl setup

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,12 @@
 (ns user
   (:require [reloaded.repl :refer [system init start stop go reset]]
-            olyp-admin.app))
+            olyp-admin.app
+            clojure.edn))
 
-(reloaded.repl/set-init! #(olyp-admin.app/create-system {:olyp-central-api {:url "http://localhost:3000"}
-                                                  :web {:port 3002}
-                                                  :env :dev
-                                                  :cookie-secret "12345678abcdef12"}))
+(reloaded.repl/set-init! #(olyp-admin.app/create-system (merge
+                                                          {:olyp-central-api {:url "http://localhost:3000"}
+                                                           :web {:port 3002}
+                                                           :env :dev
+                                                           :cookie-secret "12345678abcdef12"}
+                                                          (-> "config.edn" slurp clojure.edn/read-string))))
 
